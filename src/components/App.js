5fa6a349c52058c1e773fde16cc642611c8f2906
Kevin Grayson <k.grayson.ms@gmail.com>
2018-08-24 13:53:22 -0700

    Times up

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import pokedex from '../pokedex';
 import Pokedex from './Pokedex';
+import Pokemon from './Pokemon';
 
 class App extends Component {
 
@@ -9,16 +10,25 @@ class App extends Component {
     favorite: null
   };
 
-  handleSelect = () => {
-    console.log('Clicked!');
+  handleSelect = (pokemon) => {
+
   };
 
   render() {
     console.log(pokedex);
+    const { favorite } = this.state;
 
     return (
       <div>
         <div>Hello React Quiz! {this.state.pokedex.length}</div>
+        {favorite
+          ? <div>
+            <ul>
+              <Pokemon pokemon={favorite}/>
+            </ul>
+          </div>
+          : <p>Select a favorite pokemon</p>
+        }
         <Pokedex
           pokedex={this.state.pokedex}
           onSelect={this.handleSelect}
