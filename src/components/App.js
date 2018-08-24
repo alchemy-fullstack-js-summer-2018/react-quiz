74a7063393bf5d9d7bb7aae2cf86c709949bb4e4
MariahAdams <mariah.adams23@yahoo.com>
2018-08-24 13:25:00 -0700

    updating app

--- src/components/App.js
+++ src/components/App.js
@@ -9,14 +9,26 @@ class App extends Component {
     favorite: null
   };
 
+  // favorite passed in
+  handleSelect = () => {
+    console.log('you selected your fav pokemon!');
+  }
+
   render() {
 
     return (
       <div>
         Hello React Quiz! {this.state.pokedex.length}
 
+        <section className="favorite">
+          {pokedex
+            ? <div><img src={pokedex.url_image}></img>{pokedex.pokemon}</div>
+            : <p>please select your favorite pokemon!</p>
+          }
+        </section>
+
         <section>
-          <Pokedex />
+          <Pokedex onSelect={this.handleSelect}/>
         </section>  
       </div>
     );
